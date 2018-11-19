4b3da7f7b5d8330b4351c366f48374814060d301
Andrey <andreo.obolensky@gmail.com>
2018-11-19 07:07:09 +0300

    Add comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 
-#define LED_PIN 13
-#define BLINK_DELAY 1000
+#define LED_PIN 13 // Светодиод подключён к 13 пину
+#define BLINK_DELAY 1000 // Задержка 1000 мс
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT); // Установка режима работы на выход
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(BLINK_DELAY);
-    digitalWrite(LED_PIN, LOW);
-    delay(BLINK_DELAY);
+    digitalWrite(LED_PIN, HIGH); // Включение светодиода
+    delay(BLINK_DELAY); // Светодиод горит 1с
+    digitalWrite(LED_PIN, LOW); // Выключение светодиода
+    delay(BLINK_DELAY); // Светодиод не горит 1с
 }
